8bd59e9730f70b7a712690197c59c91496a8c97b
Muluh Joel <muluhjoel2002@gmail.com>
2022-03-05 17:50:36 +0100

    configured environment variables

--- firebase.js
+++ firebase.js
@@ -2,14 +2,13 @@ import { getApps, getApp, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDqWOMUenx1ZdirQW8oRDFxcZrF_Ft6gzA",
-  authDomain: "testapps-91ba7.firebaseapp.com",
-  projectId: "testapps-91ba7",
-  storageBucket: "testapps-91ba7.appspot.com",
-  messagingSenderId: "406257652757",
-  appId: "1:406257652757:web:51d97d7fe9c9908acc6a1c",
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
