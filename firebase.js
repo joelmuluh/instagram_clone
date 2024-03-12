790902b4500056c8aa2be28f59b21da896573e22
Joel Muluh <68227162+joelmuluh@users.noreply.github.com>
2024-03-12 18:05:40 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,16 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId,
+  apiKey: "AIzaSyB6F8vlMMCxHAX6Joge5rJVGbeHIdKvppg",
+  authDomain: "instagram-clone-ac65e.firebaseapp.com",
+  projectId: "instagram-clone-ac65e",
+  storageBucket: "instagram-clone-ac65e.appspot.com",
+  messagingSenderId: "1038262646886",
+  appId: "1:1038262646886:web:1d912fa49c42f2b40b55a7",
+  measurementId: "G-4XXTB5DSNH"
 };
+
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
+
